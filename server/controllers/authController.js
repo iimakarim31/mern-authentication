95c822426439edb9fbbe4a0b0cbec5f0ff1fa3d4
Iim Abdul Karim <iimakarim31@gmail.com>
2025-02-09 10:31:18 +0700

    create authentication usrname, email using SMTP brevo

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "../config/nodeMailer.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -29,6 +30,16 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    // Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to Alkariim Authentication App",
+      text: `Welcome to Alkariim Web Authentication App. Yout account has been created with email id: ${email}`,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     return res.json({ success: true });
   } catch (error) {
