00482f0e321e99f6bbbde3e511358f96f9b476ba
Iim Abdul Karim <iimakarim31@gmail.com>
2025-02-10 12:43:01 +0700

    create backend server to client side at login testing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,61 @@
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { assets } from "../assets/assets.js";
+import { data, useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext.jsx";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedin } = useContext(AppContent);
+
   const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true;
+
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/auth/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (error) {
+      toast.error(data.message);
+    }
+  };
+
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
       <img
+        onClick={() => navigate("/")}
         src={assets.logo}
         alt=''
         className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'
@@ -21,11 +71,13 @@ const Login = () => {
             : "Login to your account!"}
         </p>
 
-        <form action=''>
+        <form onSubmit={onSubmitHandler} action=''>
           {state === "Sign Up" && (
             <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
               <img src={assets.person_icon} alt='' />
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className='bg-transparent outline-none'
                 type='text'
                 placeholder='Full name'
@@ -37,6 +89,8 @@ const Login = () => {
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.mail_icon} alt='' />
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className='bg-transparent outline-none'
               type='email'
               placeholder='Email Id'
@@ -46,6 +100,8 @@ const Login = () => {
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
             <img src={assets.lock_icon} alt='' />
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className='bg-transparent outline-none'
               type='password'
               placeholder='Password'
@@ -53,7 +109,10 @@ const Login = () => {
             />
           </div>
 
-          <p className='mb-4 text-indigo-500 cursor-pointer'>
+          <p
+            onClick={() => navigate("/reset-password")}
+            className='mb-4 text-indigo-500 cursor-pointer'
+          >
             Forget Password?
           </p>
 
